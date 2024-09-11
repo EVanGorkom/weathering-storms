b9676e777f968362a373b3d99e90501955e68d74
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-09-10 23:29:41 -0500

    Update the types.ts file to remove the unnecessary lat and lon values for the searched city

--- src/pages/CurrentWeather/types.ts
+++ src/pages/CurrentWeather/types.ts
@@ -1,12 +1,25 @@
-// type CurrentWeather = {}
 export interface CurrentWeatherDto {
+  weather: [
+    {
+      main: string;
+      description: string;
+    }
+  ];
   main: {
     temp: number;
     feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    humidity: number;
   };
 }
 
 export interface CurrentWeather {
+  main: string;
+  description: string;
   temperature: number;
   feelsLike: number;
+  tempMin: number;
+  tempMax: number;
+  humidity: number;
 }
