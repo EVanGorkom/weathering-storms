dc85fd4377559fa060c9e2adf00008ccfb86bb53
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-09-10 23:32:36 -0500

    Create new logic to process a user's device's location data for creating location based on the user's location as a default and create create the logic to make the display hidden until the page is given data.

--- src/pages/CurrentWeather/index.tsx
+++ src/pages/CurrentWeather/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { CurrentWeather } from './types';
 import { mapCurrentWeather } from '../../utils/mapCurrentWeather';
 import { SearchField } from '../../components/search_field/searchFind';
@@ -7,37 +7,106 @@ import { Loading } from '../../components/loading/loading'
 
 function CurrentWeatherDisplay() {
   const initialWeatherState: CurrentWeather = {
+    main: "",
+    description: "",
     temperature: 0,
     feelsLike: 0,
+    tempMin: 0,
+    tempMax: 0,
+    humidity: 0,
   };
   const [weather, setWeather] = useState<CurrentWeather>(initialWeatherState);
   const [searchValue, setSearchValue] = useState<string>('');
   const [isLoading, setIsLoading] = useState(false);
-  const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+  const [locationAllowed, setLocationAllowed] = useState(false);
+  const CURRENT_WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+  // const WEEKLY_WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/forecast/daily";
   const API_KEY = import.meta.env.VITE_API_KEY;
 
-  const fetchWeatherBySearch = (city: string, units: string = "imperial") => {
-    setIsLoading(true)
+
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setLocationAllowed(true)
+          fetchWeatherByDeviceLocation(latitude, longitude);
+        },
+        (error) => {
+          console.error("Error acquiring device location: ", error);
+          setLocationAllowed(false);
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported by this browser.")
+      setLocationAllowed(false);
+    }
+  };
+
+  const fetchWeatherByDeviceLocation = (lat: number, lon: number, units: string = "imperial") => {
+    setIsLoading(true);
     fetch(
-      `${BASE_URL}?q=${city}&units=${units}&appid=${API_KEY}`,
+      `${CURRENT_WEATHER_BASE_URL}?lat=${lat}&lon=${lon}&units=${units}&appid=${API_KEY}`,
       {
-        method: 'GET',
+        method: "GET"
       })
       .then((response) => response.json())
       .then((data) => {
         const currentWeather = mapCurrentWeather(data);
+        console.log(weather);
         setWeather(currentWeather);
-        setIsLoading(false)
+        setIsLoading(false);
       })
       .catch((error) => console.log(error));
+      setIsLoading(false);
   };
+
+  const fetchWeatherBySearch = (city: string, units: string = "imperial") => {
+    setIsLoading(true);
+    fetch(
+      `${CURRENT_WEATHER_BASE_URL}?q=${city}&units=${units}&appid=${API_KEY}`,
+      {
+        method: "GET",
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        const currentWeather = mapCurrentWeather(data);
+        console.log(weather);
+        setWeather(currentWeather);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+      setIsLoading(false);
+  };
+
+  const fetchForecastBySearch = (city: string, units: string = "imperial") => {
+    setIsLoading(true);
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${API_KEY}`,
+      {
+        method: 'GET',
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
 
   if (isLoading) {
     return <Loading />;
-  }
+  };
 
   const handleSearch = () => {
     fetchWeatherBySearch(searchValue);
+    fetchForecastBySearch(searchValue);
   };
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -47,9 +116,34 @@ function CurrentWeatherDisplay() {
 
   return (
     <>
-      <SearchField inputValue={searchValue} inputOnChange={handleSearchChange} buttonOnClick={handleSearch}/>
-      <div>Current Temperature: {weather.temperature}</div>
-      <div>Feels Like: {weather.feelsLike}</div>
+      <SearchField
+        inputValue={searchValue}
+        inputOnChange={handleSearchChange}
+        buttonOnClick={handleSearch}
+      />
+
+      {locationAllowed && weather ? (
+        <div className="current-weather-details">
+          <div>{weather.description}</div>
+          <div>Current Temperature: {weather.temperature}</div>
+          <div>Low: {weather.tempMin}</div>
+          <div>High: {weather.tempMax}</div>
+          <div>Humidity: {weather.humidity}</div>
+          <div>Feels Like: {weather.feelsLike}</div>
+        </div>
+      ) : (
+        searchValue &&
+        weather && (
+          <div className="current-weather-details">
+            <div>{weather.description}</div>
+            <div>Current Temperature: {weather.temperature}</div>
+            <div>Low: {weather.tempMin}</div>
+            <div>High: {weather.tempMax}</div>
+            <div>Humidity: {weather.humidity}</div>
+            <div>Feels Like: {weather.feelsLike}</div>
+          </div>
+        )
+      )}
     </>
   );
 }
