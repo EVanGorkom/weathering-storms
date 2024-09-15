751af1192ad2c10a406205c52211e8cacb597140
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-09-14 22:38:32 -0500

    Add logic for variable backgrounds based on the user's time of day

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react';
 import { CurrentWeatherDisplay } from './pages/CurrentWeather';
 
 function App() {
+  const [isDayTime, setIsDayTime] = useState(true);
+
+  useEffect(() => {
+    const hour = new Date().getHours();
+    setIsDayTime(hour >= 7 && hour < 19);
+  }, []);
+
   return (
     <>
-      <div className="min-h-screen bg-gradient-to-b from-blue-400 via-blue-500 to-blue-900">
+      <div className={`min-h-screen ${
+        isDayTime
+          ? 'bg-gradient-to-b from-blue-400 via-blue-500 to-blue-900'
+          : 'bg-gradient-to-b from-purple-700 to-indigo-950'
+        }`}>
         <h1 className="content-center text-center p-4 text-6xl text-white font-serif">
           Weathering Storms
         </h1>
